4dbfd53ba7ec28b4ce640014ea182c5c3876c572
malykhan <92220660+malykhan234@users.noreply.github.com>
2022-02-12 10:54:50 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,79 @@
+// let num=3
+// console.log(num);
+
+
+// 2
+ 
+// let a=10, b=2
+// console.log(a+b, a-b, a*b, a/b);
+
+// 3
+// let c=15, d=2, result=c+d
+// console.log(c+d);
+
+//4
+// let a=10, b=2, c=5
+// console.log(a+b+c);
+
+//5
+// let a=17, b=10, c=a-b,d=7 ,result=c+d
+// console.log(result);
+
+//6
+// let str ='ПриветМир!'
+// console.log('ПриветМир!');
+
+// 7
+// let str1='Привет,', str2='Мир!'
+// console.log(str1+str2)
+
+//8
+// let name='bogdan' 
+// console.log(`hello ${name}`);
+
+//9
+// let age=98
+// console.log(`Мне ${age} лет`);
+
+//10
+// let num = prompt("СКАЖИ ЧИСЛО УРОД", 288)
+// alert=(num)
+
+//11
+// let num = prompt("СКАЖИ ЧИСЛО УРОД", 288)
+// alert=(num)
+
+
+//12
+// let str="abcde"
+// console.log(str[0], str[2], str[3], str[4] );
+
+//13
+// let str="abcde"
+// console.log(str[0], str[2], str[3], str[4] );
+
+//14
+// console.log(`В минуте ${60} секунд, в одном часе ${60*60} секунд, в одном дне ${60*60*24} секунд.`)
+
+//15
+// let hour = 15, minute = 32, second = 22
+// console.log(`${hour}:${minute}:${second}`)
+
+//16
+// let num=17
+// alert(num ** 2)
+
+//17
+// let num = 47;
+// num += 7;
+// num -= 18;
+// num *= 10;
+// num /= 15;
+// alert(num);
+
+//18
+// var num = 10;
+// num  ++;
+// num ++;
+// num --;
+// alert(num);
